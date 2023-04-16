2e8a1844621bac8ac364b4323a6dcee3271c2c45
Lisa <45350703+brbiegrl@users.noreply.github.com>
2023-04-17 00:18:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,4 @@ import numpy as np
 chat_id = 909631698 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    t = 10; mu = -13; sigma = np.exp(1)
-    dist = np.random.norm(mu, sigma, len(x))
-    return dist.mean()/t
+    return (x.mean() + 12) / t
